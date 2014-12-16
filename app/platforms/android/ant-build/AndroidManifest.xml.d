ea7cea311b0de81a1aaf6246fddb4db083cbdd7c
NRxl <nathan.rouxel@gmail.com>
2014-12-16 13:01:30 +0100

    Ajout du build dans app

---
+++ app/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/sources_prelib/app/platforms/android/ant-build/AndroidManifest.xml : \
+/home/sources_prelib/app/platforms/android/AndroidManifest.xml \
---
+++ app/platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,3 @@
+//angular.module('Listcontroller', [])
+
+"use strict";

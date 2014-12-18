bfe2910ba5c65c5bf825d605e72041ec54b0ed1f
NRxl <nathan.rouxel@gmail.com>
2014-12-18 19:55:02 +0100

    Mise en ligne de l'application build avec le bouton droit à côté des stations menant vers une page vide.

--- app/platforms/android/assets/www/cordova_plugins.js
+++ app/platforms/android/assets/www/cordova_plugins.js
@@ -19,7 +19,6 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "com.ionic.keyboard": "1.0.3",
-    "org.apache.cordova.console": "0.2.11",
     "org.apache.cordova.device": "0.2.12"
 }
 // BOTTOM OF METADATA
--- app/platforms/android/assets/www/js/controllers.js
+++ app/platforms/android/assets/www/js/controllers.js
@@ -1,3 +1,2 @@
 //angular.module('Listcontroller', [])
 
-"use strict";

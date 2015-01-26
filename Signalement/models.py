6d015031e25aa90502dd5df75dbf6d738edc7444
pesneaut <thomas.pesneau@eleves.enpc.fr>
2015-01-26 17:30:14 +0100

    Replaced station_name by station_id

--- Signalement/models.py
+++ Signalement/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Global(models.Model):
-    station_name = models.CharField(max_length=200)
+    station_id = models.IntegerField(default=0)
     report_date = models.DateTimeField(auto_now_add=True,auto_now=False,
                                        verbose_name='report date')
     station_loss = models.IntegerField('Broken bikes',default=0)
     def __str__(self):
-        return self.station_name
+        return self.station_id
 
     
     

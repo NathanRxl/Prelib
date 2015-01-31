7f14718b6390f069a8ec6365cd6c40878a07f689
NRxl <nathan.rouxel@gmail.com>
2015-01-31 12:10:23 +0100

    Mise à jour du dépôt GitHub par rapport au dépôt Heroku: ajout de Create_report.py qui gère la requête envoyée depuis l'application mobile

--- Prelib/urls.py
+++ Prelib/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^Signalement/',include('Signalement.urls')),                       
+    # Examples:
+    # url(r'^$', 'Prelib.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
 )
--- Signalement/views.py
+++ Signalement/views.py
@@ -1,17 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-import django.http
-from models import Global
-
-#Insert a row in the table Global with an instance of global
-def create_report(request):
-    if request.method == 'GET':
-        pass
-    elif request.method == 'POST':
-        request.POST.get(name,loss)#get the name of the station and
-        #the number of broken bikes
-        report = Global(station_name = name, station_loss = loss)
-        #create a row and save it
-        report.save()
-        

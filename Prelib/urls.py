682c1998b9e105172651d86e55080823eaf9a86f
pesneaut <thomas.pesneau@eleves.enpc.fr>
2015-02-02 17:36:46 +0100

    correction d'une petite erreur de regex

--- Prelib/urls.py
+++ Prelib/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^Signalement/',include('Signalement.urls'))
-    url(r'admin/',include(admin.site.urls))
+    url(r'^admin/',include(admin.site.urls))
 )
                        

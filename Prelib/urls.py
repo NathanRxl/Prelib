960c74fce6efa9bd09d7ad7c60f835780302008b
pesneaut <thomas.pesneau@eleves.enpc.fr>
2015-02-02 17:22:19 +0100

    Adaptation de l'url

--- Prelib/urls.py
+++ Prelib/urls.py
@@ -7,4 +7,4 @@ urlpatterns = patterns('',
     # url(r'^$', 'Prelib.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$Signalement/',include('Signalement.urls')))
+    url(r'^Signalement/',include('Signalement.urls')))

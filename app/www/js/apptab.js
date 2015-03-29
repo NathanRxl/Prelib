dc247e7bf09a8d1d280188240b21c354d427a83f
mathieue <emile.mathieu@eleves.enpc.fr>
2015-03-29 23:44:29 +0200

    trying to fix map backroung not showing

--- app/www/js/apptab.js
+++ app/www/js/apptab.js
@@ -312,7 +312,7 @@ app.controller("MapCtrl", function($scope,VelibAPI) {
     
     var map = L.map('map');
 
-    L.tileLayer('http://{s}.tiles.mapbox.com/v3/emilemathieu.lhni69mg/{z}/{x}/{y}.png', {
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18
     }).addTo(map);
